20e96d5ebe9297c9d99fa36afa7daa1452662a9c
Milan Kocourek <m.kocourek@gmail.com>
2023-01-27 13:00:55 +0100

    Upravy na dockerech a admineru

--- dockerfiles/deployer/docker-entrypoint.sh
+++ dockerfiles/deployer/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir /root/.ssh
 cp /tmp/.ssh/id_rsa_nopass /root/.ssh/id_rsa
-cp /tmp/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub
+# cp /tmp/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub
 cp /tmp/.ssh/known_hosts /root/.ssh/known_hosts
 
 chmod 700 /root/.ssh
---
+++ stacks/base/adminer/AdminerFloatTheadWrapper.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/../plugins/floatThead.php';
+
+return new AdminerFloatThead(null, 'plugins/jquery/jquery.floatThead.min.js'); // null, nenacitat jQuery, uz je nacteno!
