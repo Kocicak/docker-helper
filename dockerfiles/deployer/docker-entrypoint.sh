2de2d0eb88559faae32a11cf32534913f5799031
Milan Kocourek <m.kocourek@gmail.com>
2025-01-29 16:21:55 +0100

    V rootless dockeru nefunguje xdebug. Takze se vracim zpatky k puvodnimu reseni. zmena konfigurace mariadb, update ssh deployeru - novy ssh klic

--- dockerfiles/deployer/docker-entrypoint.sh
+++ dockerfiles/deployer/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 set -e
 
 mkdir /root/.ssh
-cp /tmp/.ssh/id_rsa_nopass /root/.ssh/id_rsa
+cp /tmp/.ssh/id_ed25519 /root/.ssh/id_ed25519
 # cp /tmp/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub
 cp /tmp/.ssh/known_hosts /root/.ssh/known_hosts
 
 chmod 700 /root/.ssh
 # chmod 644 /root/.ssh/id_rsa.pub
-chmod 600 /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_ed25519
 
 deployer "$@"
