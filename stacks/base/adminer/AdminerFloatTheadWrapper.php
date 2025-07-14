475784a3b1cc9d143d15337efba5cadc881146a3
Milan Kocourek <m.kocourek@gmail.com>
2025-07-14 10:54:17 +0200

    Novejsi adminer, upravy pluginu

--- stacks/base/adminer/AdminerFloatTheadWrapper.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../plugins/floatThead.php';
-
-return new AdminerFloatThead(null, 'plugins/jquery/jquery.floatThead.min.js'); // null, nenacitat jQuery, uz je nacteno!
